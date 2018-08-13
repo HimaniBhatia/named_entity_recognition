06081267172c2d46746ea43067349645ac067b1d
Himani Bhatia <himanibhatia0907@gmail.com>
2018-08-13 22:55:53 +0530

    modified the cli parser to accept tags and words

--- cli_parser.py
+++ cli_parser.py
@@ -6,22 +6,13 @@ parser = argparse.ArgumentParser(
 
 # add required argument
 parser.add_argument(
-    'query', 
-    help="The tag/word with which to recognize"
+    'tag', 
+    help="Required. The tag with which to recognize"
 )
 
 # add a flag that marks the query as a TAG
 parser.add_argument(
-    '--tag', 
-    dest='is_tag',
-    action='store_true',
-    help="Marks the input as a tag. If not given, --word is required"
-)
-
-# add a flag that marks the query as a WORD
-parser.add_argument(
-    '--word', 
-    dest='is_word',
-    action='store_true',
-    help="Marks the input as a word. If not given, --tag is required"
+    'word', 
+    nargs='?',
+    help="Optional. The tag with which to refine the query"
 )
--- main.py
+++ main.py
@@ -4,18 +4,12 @@ def main():
 
     # parse all the command line arguments
     args = parser.parse_args()
-
-    # validate the path passed in the argument
-    if(args.is_tag and args.is_word):
-        arge.error("Both --tag and --word cannot be used together")
-
-    if(not args.is_tag and not args.is_word):
-        arge.error("Either --tag or --word is required")
-
-    if(args.is_tag):
-        # code
-    else:
-        # code
+    tag = None
+    word = None
+    if args.tag:
+        tag = args.tag
+    if args.word:
+        word = args.word
 
 if __name__ == '__main__':
   main()
